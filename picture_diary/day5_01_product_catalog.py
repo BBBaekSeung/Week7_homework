56f780f030c72f872b30ce5a5345be7a28be092f
EunSeo Rhee <128893466+BBBaekSeung@users.noreply.github.com>
2026-05-30 00:02:45 +0900

    Add day5_01_product_catalog.py

---
+++ picture_diary/day5_01_product_catalog.py
@@ -0,0 +1,11 @@
+shot_map = {
+    "small": "close-up shot",
+    "medium": "medium shot",
+    "large": "wide shot"
+}
+
+angle_map = {
+    "round": "front angle",
+    "square": "eye-level angle",
+    "long": "side angle"
+}
